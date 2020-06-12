8f70bc465778fe6f54578acb78230193be8669c0
Josh Gubler <josh.gubler@gmail.com>
2020-06-12 13:19:57 -0600

    add cpy and stg envs

---
+++ terraform-iac/cpy/app/main.tf
@@ -0,0 +1,34 @@
+terraform {
+  backend "s3" {
+    bucket         = "terraform-state-storage-<account_number>"
+    dynamodb_table = "terraform-state-lock-<account_number>"
+    key            = "hw-lambda-api-cpy/app.tfstate"
+    region         = "us-west-2"
+  }
+}
+
+provider "aws" {
+  version = "~> 2.42"
+  region  = "us-west-2"
+}
+
+module "app" {
+  source = "../../modules/app/"
+  env    = "cpy"
+}
+
+output "url" {
+  value = module.app.url
+}
+
+output "codedeploy_app_name" {
+  value = module.app.codedeploy_app_name
+}
+
+output "codedeploy_deployment_group_name" {
+  value = module.app.codedeploy_deployment_group_name
+}
+
+output "codedeploy_appspec_json_file" {
+  value = module.app.codedeploy_appspec_json_file
+}
---
+++ terraform-iac/cpy/setup/setup.tf
@@ -0,0 +1,24 @@
+terraform {
+  backend "s3" {
+    bucket         = "terraform-state-storage-<account_number>"
+    dynamodb_table = "terraform-state-lock-<account_number>"
+    key            = "hw-lambda-api-cpy/setup.tfstate"
+    region         = "us-west-2"
+  }
+}
+
+provider "aws" {
+  version = "~> 2.42"
+  region  = "us-west-2"
+}
+
+variable "some_secret" {
+  type        = string
+  description = "Some secret string that will be stored in SSM for the Lambda to access at runtime."
+}
+
+module "setup" {
+  source      = "../../modules/setup/"
+  env         = "cpy"
+  some_secret = var.some_secret
+}
