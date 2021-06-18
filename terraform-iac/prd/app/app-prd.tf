4fa02b055c39d886fb88a2b67e4a22ee92eca849
yoshutch <yoshutch@gmail.com>
2021-06-18 09:54:28 -0600

    upgraded terraform to 1.0.0 and renamed files and using default tags

--- terraform-iac/prd/app/app-prd.tf
+++ terraform-iac/prd/app/app-prd.tf
@@ -1,15 +1,15 @@
 terraform {
-  required_version = "0.14.7" # must match value in .github/workflows/*.yml
+  required_version = "1.0.0" # must match value in .github/workflows/*.yml
   backend "s3" {
     bucket         = "terraform-state-storage-539738229445"
     dynamodb_table = "terraform-state-lock-539738229445"
-    key            = "hw-lambda-api/cpy/app.tfstate"
+    key            = "hw-lambda-api/prd/app.tfstate"
     region         = "us-west-2"
   }
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "~> 3.0"
+      version = "~> 3.38.0"
     }
     local = {
       source  = "hashicorp/local"
@@ -18,15 +18,28 @@ terraform {
   }
 }
 
+locals {
+  repo_name = "hw-lambda-api"
+  env       = "prd"
+}
+
 provider "aws" {
   region = "us-west-2"
+  default_tags {
+    tags = {
+      env              = local.env
+      data-sensitivity = "public"
+      repo             = "https://github.com/byu-oit/${local.repo_name}"
+    }
+  }
 }
 
 module "app" {
   source                          = "../../modules/app/"
-  env                             = "cpy"
-  deploy_test_postman_collection  = "../../../.postman/hw-lambda-api.postman_collection.json"
-  deploy_test_postman_environment = "../../../.postman/cpy-tst.postman_environment.json"
+  repo_name                       = local.repo_name
+  env                             = local.env
+  deploy_test_postman_collection  = "../../../.postman/${local.repo_name}.postman_collection.json"
+  deploy_test_postman_environment = "../../../.postman/${local.env}-tst.postman_environment.json"
 }
 
 output "url" {
--- terraform-iac/stg/setup/setup-stg.tf
+++ terraform-iac/stg/setup/setup-stg.tf
@@ -2,19 +2,31 @@ terraform {
   backend "s3" {
     bucket         = "terraform-state-storage-977306314792"
     dynamodb_table = "terraform-state-lock-977306314792"
-    key            = "hw-lambda-api/dev/setup.tfstate"
+    key            = "hw-lambda-api/stg/setup.tfstate"
     region         = "us-west-2"
   }
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "~> 3.0"
+      version = "~> 3.38.0"
     }
   }
 }
 
+locals {
+  repo_name = "hw-lambda-api"
+  env       = "stg"
+}
+
 provider "aws" {
   region = "us-west-2"
+  default_tags {
+    tags = {
+      env              = local.env
+      data-sensitivity = "public"
+      repo             = "https://github.com/byu-oit/${local.repo_name}"
+    }
+  }
 }
 
 variable "some_secret" {
@@ -24,6 +36,7 @@ variable "some_secret" {
 
 module "setup" {
   source      = "../../modules/setup/"
-  env         = "dev"
+  repo_name   = local.repo_name
+  env         = local.env
   some_secret = var.some_secret
 }
