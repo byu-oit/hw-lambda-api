ab0f4430f5cb78bcc6e47a3c1ffdf2d7641f5f77
Josh Gubler <jgubler@byu.edu>
2025-02-10 09:37:38 -0700

    increase postman timeout

--- terraform-iac/modules/app/main.tf
+++ terraform-iac/modules/app/main.tf
@@ -201,6 +201,7 @@ EOF
 module "postman_test_lambda" {
   source   = "github.com/byu-oit/terraform-aws-postman-test-lambda?ref=v6.0.0"
   app_name = "${var.repo_name}-${var.env}"
+  timeout  = 60
   postman_collections = [
     {
       collection  = var.deploy_test_postman_collection
