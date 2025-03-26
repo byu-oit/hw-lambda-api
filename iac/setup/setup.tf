953a5638e4d957334b6eb54ed88828bffab2173a
Gary Crye <garygsc@gmail.com>
2025-03-26 13:13:31 -0600

    Refactor directory structure

---
+++ iac/setup/setup.tf
@@ -0,0 +1,82 @@
+terraform {
+  required_version = "~> 1.9"
+  backend "s3" {
+    bucket         = "terraform-state-storage-${var.aws_account_id}"
+    dynamodb_table = "terraform-state-lock-${var.aws_account_id}"
+    key            = "${local.name}/${var.env}/setup.tfstate"
+    encrypt        = true
+    region         = "us-west-2"
+  }
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.70"
+    }
+    local = {
+      source  = "hashicorp/local"
+      version = "~> 2.5"
+    }
+  }
+}
+
+provider "aws" {
+  region = "us-west-2"
+
+  allowed_account_ids = [var.aws_account_id]
+
+  default_tags {
+    tags = {
+      # app                    = local.name
+      repo                   = "https://github.com/${local.gh_org}/${local.gh_repo}"
+      data-sensitivity       = "public"
+      env                    = var.env
+      # resource-creator-email = "GitHub-Actions"
+    }
+  }
+}
+
+variable "env" {
+  type = string
+}
+
+variable "aws_account_id" {
+  type        = string
+  description = "The 12-digit number that uniquely identifies an AWS account."
+}
+
+variable "some_secret" {
+  type = string
+}
+
+locals {
+  name    = "hw-lambda-api"
+  gh_org  = "byu-oit"
+  gh_repo = "hw-lambda-api"
+}
+
+module "acs" {
+  source = "github.com/byu-oit/terraform-aws-acs-info?ref=v4.0.0"
+}
+
+resource "aws_ssm_parameter" "some_secret" {
+  name  = "/${local.name}/${var.env}/some-secret"
+  type  = "SecureString"
+  value = var.some_secret
+
+  lifecycle {
+    ignore_changes = [value]
+  }
+}
+
+module "gha_role" {
+  source                         = "terraform-aws-modules/iam/aws//modules/iam-assumable-role-with-oidc"
+  version                        = "5.44.0"
+  create_role                    = true
+  role_name                      = "${local.name}-${var.env}-gha"
+  provider_url                   = module.acs.github_oidc_provider.url
+  role_permissions_boundary_arn  = module.acs.role_permissions_boundary.arn
+  role_policy_arns               = module.acs.power_builder_policies[*].arn
+  oidc_fully_qualified_audiences = ["sts.amazonaws.com"]
+  oidc_subjects_with_wildcards   = ["repo:${local.gh_org}/${local.gh_repo}:*"]
+}
+
